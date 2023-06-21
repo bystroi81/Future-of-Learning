ca5df94b843baaee747d3294b2002c4a95aa5958
bystroi81 <136693674+bystroi81@users.noreply.github.com>
2023-06-21 14:02:57 +0700

    Add support for mobile devices

---
+++ yellow_elephant_waterfall.js
@@ -0,0 +1,517 @@
+//1
+let learningMethodology = "Adaptive Learning";
+
+//2
+let studentsExpectedOutcome = "Personalized Learning Experience";
+
+//3
+let learningsFuture = document.createElement("div");
+learningsFuture.className = "learning-future";
+
+//4
+let demonstrateLearnersAbility = document.createElement("div");
+demonstrateLearnersAbility.className = "showcase-abilities";
+
+//5
+let abilityToApplyKnowledge = document.createElement("div");
+abilityToApplyKnowledge.className = "applicability";
+
+//6
+let dataDrivenAssessment = document.createElement("div");
+dataDrivenAssessment.className = "data-driven";
+
+//7
+let assessmentTools = document.createElement("div");
+assessmentTools.className = "assess-tool";
+
+//8
+function generateLearningPathway() {
+  let learningPathway = document.createElement("div");
+  learningPathway.className = "learning-pathway";
+
+  return learningPathway;
+}
+
+//9
+function lookForOpportunitiesToScale() {
+  let scaleUpLearning = document.createElement("div");
+  scaleUpLearning.className = "scale-learning";
+
+  return scaleUpLearning;
+}
+
+//10
+function trackProgressAndAnalytics() {
+  let progressTracker = document.createElement("div");
+  progressTracker.className = "track-progress";
+
+  return progressTracker;
+}
+
+//11
+function createBranchedLearningExperiences() {
+  let branchedLearning = document.createElement("div");
+  branchedLearning.className = "branched-learning";
+
+  return branchedLearning;
+}
+
+//12
+function useAIForAdaptiveLearning() {
+  let AIAdaptiveLearning = document.createElement("div");
+  AIAdaptiveLearning.className = "ai-adaptive-learning";
+
+  return AIAdaptiveLearning;
+}
+
+//13
+function utilizePredictiveAnalytics() {
+  let predictiveAnalytics = document.createElement("div");
+  predictiveAnalytics.className = "predictive-analytics";
+
+  return predictiveAnalytics;
+}
+
+//14
+function allowRealTimeInteractions() {
+  let realTimeInteraction = document.createElement("div");
+  realTimeInteraction.className = "real-time-interaction";
+
+  return realTimeInteraction;
+}
+
+//15
+function integrateGraphicsAndVideos() {
+  let graphicVideosIntegration = document.createElement("div");
+  graphicVideosIntegration.className = "graphics-videos";
+
+  return graphicVideosIntegration;
+}
+
+//16
+function useChatbotsForInstantFeedback() {
+  let chatbotFeedback = document.createElement("div");
+  chatbotFeedback.className = "chatbot-feedback";
+
+  return chatbotFeedback;
+}
+
+//17
+function programmaticallyUpdateContent() {
+  let contentUpdater = document.createElement("div");
+  contentUpdater.className = "content-updater";
+
+  return contentUpdater;
+}
+
+//18
+function rewardCollaborativeLearning() {
+  let collaborativeRewards = document.createElement("div");
+  collaborativeRewards.className = "collaborative-rewards";
+
+  return collaborativeRewards;
+}
+
+//19
+function connectWithExperts() {
+  let expertConnections = document.createElement("div");
+  expertConnections.className = "expert-connections";
+
+  return expertConnections;
+}
+
+//20
+function useGamificationToMotivateStudents() {
+  let gameMotivation = document.createElement("div");
+  gameMotivation.className = "game-motivation";
+
+  return gameMotivation;
+}
+
+//21
+function embedRealWorldScenarios() {
+  let realWorldScenarios = document.createElement("div");
+  realWorldScenarios.className = "real-world-scenarios";
+
+  return realWorldScenarios;
+}
+
+//22
+function applyContentToRealLife() {
+  let lifeContentApplication = document.createElement("div");
+  lifeContentApplication.className = "life-content-app";
+
+  return lifeContentApplication;
+}
+
+//23
+function useVirtualAndAugmentedReality() {
+  let virtualARReality = document.createElement("div");
+  virtualARReality.className = "virtual-ar-reality";
+
+  return virtualARReality;
+}
+
+//24
+function engageLearnersWithRobotics() {
+  let learnerRobotics = document.createElement("div");
+  learnerRobotics.className = "learner-robotics";
+
+  return learnerRobotics;
+}
+
+//25
+function connectLearnersWithSimilarInterests() {
+  let interestConnections = document.createElement("div");
+  interestConnections.className = "interest-connections";
+
+  return interestConnections;
+}
+
+//26
+function introduceAIInLearningProcess() {
+  let AIInLearning = document.createElement("div");
+  AIInLearning.className = "ai-in-learning";
+
+  return AIInLearning;
+}
+
+//27
+function moveAwayFromOneSizeFitsAllLearning() {
+  let personalisedLearning = document.createElement("div");
+  personalisedLearning.className = "personalised-learning";
+
+  return personalisedLearning;
+}
+
+//28
+function useInteractiveLearningTools() {
+  let interactiveTools = document.createElement("div");
+  interactiveTools.className = "interactive-tools";
+
+  return interactiveTools;
+}
+
+//29
+function improveReadingComprehension() {
+  let readingComprehension = document.createElement("div");
+  readingComprehension.className = "reading-comprehension";
+
+  return readingComprehension;
+}
+
+//30
+function reduceTimeForInpersonTraining() {
+  let inPersonTraining = document.createElement("div");
+  inPersonTraining.className = "in-person-training";
+
+  return inPersonTraining;
+}
+
+//31
+function useAIForPersonalizedMentorship() {
+  let personalizedMentorship = document.createElement("div");
+  personalizedMentorship.className = "personalized-mentorship";
+
+  return personalizedMentorship;
+}
+
+//32
+function useAIForAssessment() {
+  let AIForAssessment = document.createElement("div");
+  AIForAssessment.className = "ai-for-assessment";
+
+  return AIForAssessment;
+}
+
+//33
+function developComputerVisionApps() {
+  let computerVisionApps = document.createElement("div");
+  computerVisionApps.className = "computer-vision-apps";
+
+  return computerVisionApps;
+}
+
+//34
+function utilizeNeuralNetworks() {
+  let neuralNetworks = document.createElement("div");
+  neuralNetworks.className = "neural-networks";
+
+  return neuralNetworks;
+}
+
+//35
+function integrateCognitiveTechnologies() {
+  let cognitiveTech = document.createElement("div");
+  cognitiveTech.className = "cognitive-tech";
+
+  return cognitiveTech;
+}
+
+//36
+function offerFlexibleLearningOptions() {
+  let flexibleLearningOptions = document.createElement("div");
+  flexibleLearningOptions.className = "flexible-learning-options";
+
+  return flexibleLearningOptions;
+}
+
+//37
+function encourageLearnerDiscovery() {
+  let learnerDiscovery = document.createElement("div");
+  learnerDiscovery.className = "learner-discovery";
+
+  return learnerDiscovery;
+}
+
+//38
+function buildLifelongLearningCapabilities() {
+  let lifelongLearning = document.createElement("div");
+  lifelongLearning.className = "lifelong-learning";
+
+  return lifelongLearning;
+}
+
+//39
+function provideSocialLearningOpportunities() {
+  let socialLearningOpportunities = document.createElement("div");
+  socialLearningOpportunities.className = "social-learning";
+
+  return socialLearningOpportunities;
+}
+
+//40
+function useDigitalSimulationsForOnlineClasses() {
+  let digitalSimulations = document.createElement("div");
+  digitalSimulations.className = "digital-simulations";
+
+  return digitalSimulations;
+}
+
+//41
+function increaseComputationalThinkingSkills() {
+  let computationalSkills = document.createElement("div");
+  computationalSkills.className = "computational-skills";
+
+  return computationalSkills;
+}
+
+//42
+function supportSelfDirectedLearning() {
+  let selfDirectedLearning = document.createElement("div");
+  selfDirectedLearning.className = "self-directed-learning";
+
+  return selfDirectedLearning;
+}
+
+//43
+function useAIForRealTimeFeedback() {
+  let realTimeFeedback = document.createElement("div");
+  realTimeFeedback.className = "real-time-feedback";
+
+  return realTimeFeedback;
+}
+
+//44
+function useAdaptiveLearningTechniques() {
+  let adaptiveLearning = document.createElement("div");
+  adaptiveLearning.className = "adaptive-learning";
+
+  return adaptiveLearning;
+}
+
+//45
+function allowLearnersToTeachThemselves() {
+  let selfTeaching = document.createElement("div");
+  selfTeaching.className = "self-teaching";
+
+  return selfTeaching;
+}
+
+//46
+function utilizeInteractiveStorytelling() {
+  let interactiveStorytelling = document.createElement("div");
+  interactiveStorytelling.className = "interactive-storytelling";
+
+  return interactiveStorytelling;
+}
+
+//47
+function leverageCloudTechnology() {
+  let cloudTech = document.createElement("div");
+  cloudTech.className = "cloud-tech";
+
+  return cloudTech;
+}
+
+//48
+function incorporateRealTimeData() {
+  let realTimeData = document.createElement("div");
+  realTimeData.className = "real-time-data";
+
+  return realTimeData;
+}
+
+//49
+function usePersonalizedContentDelivery() {
+  let contentDelivery = document.createElement("div");
+  contentDelivery.className = "personalized-content-delivery";
+
+  return contentDelivery;
+}
+
+//50
+function utilizeDataMiningTechniques() {
+  let dataMining = document.createElement("div");
+  dataMining.className = "data-mining";
+
+  return dataMining;
+}
+
+//51
+function useSoftwareRobotsForAutomation() {
+  let automationSoftwareRobots = document.createElement("div");
+  automationSoftwareRobots.className = "software-robots-automation";
+
+  return automationSoftwareRobots;
+}
+
+//52
+function useNaturalLanguageProcessing() {
+  let NLP = document.createElement("div");
+  NLP.className = "natural-language-processing";
+
+  return NLP;
+}
+
+//53
+function createInteractiveLessonPlans() {
+  let interactiveLessonPlans = document.createElement("div");
+  interactiveLessonPlans.className = "interactive-lesson-plans";
+
+  return interactiveLessonPlans;
+}
+
+//54
+function useArtificialIntelligenceInEducation() {
+  let AIInEd = document.createElement("div");
+  AIInEd.className = "ai-in-education";
+
+  return AIInEd;
+}
+
+//55
+function provideMultimediaContent() {
+  let multimediaContent = document.createElement("div");
+  multimediaContent.className = "multimedia-content";
+
+  return multimediaContent;
+}
+
+//56
+function allowLearnersToCreateContent() {
+  let contentCreation = document.createElement("div");
+  contentCreation.className = "content-creation";
+
+  return contentCreation;
+}
+
+//57
+function moveTowardsSmartClassrooms() {
+  let smartClassrooms = document.createElement("div");
+  smartClassrooms.className = "smart-classrooms";
+
+  return smartClassrooms;
+}
+
+//58
+function useAIForContentCuration() {
+  let contentCuration = document.createElement("div");
+  contentCuration.className = "content-curation";
+
+  return contentCuration;
+}
+
+//59
+function introduceMultimodalLearning() {
+  let multimodalLearning = document.createElement("div");
+  multimodalLearning.className = "multimodal-learning";
+
+  return multimodalLearning;
+}
+
+//60
+function giveRealWorldExperience() {
+  let realWorldExperience = document.createElement("div");
+  realWorldExperience.className = "real-world-experience";
+
+  return realWorldExperience;
+}
+
+//61
+function focusOnGrowingFutureSkills() {
+  let futureSkills = document.createElement("div");
+  futureSkills.className = "future-skills";
+
+  return futureSkills;
+}
+
+//62
+function createRecommendationSystems() {
+  let recommendationSystems = document.createElement("div");
+  recommendationSystems.className = "recommendation-systems";
+
+  return recommendationSystems;
+}
+
+//63
+function makeUseOfVoiceRecognitionSystems() {
+  let voiceRecognition = document.createElement("div");
+  voiceRecognition.className = "voice-recognition";
+
+  return voiceRecognition;
+}
+
+//64
+function supportCollaborativeLearningProjects() {
+  let collaborativeProjects = document.createElement("div");
+  collaborativeProjects.className = "collaborative-projects";
+
+  return collaborativeProjects;
+}
+
+//65
+function identifyLearningGaps() {
+  let learningGaps = document.createElement("div");
+  learningGaps.className = "learning-gaps";
+
+  return learningGaps;
+}
+
+//66
+function createPersonalizedCurriculums() {
+  let personalizedCurriculums = document.createElement("div");
+  personalizedCurriculums.className = "personalized-curriculums";
+
+  return personalizedCurriculums;
+}
+
+//67
+function developRobustOnlineCommunities() {
+  let onlineCommunities = document.createElement("div");
+  onlineCommunities.className = "online-communities";
+
+  return onlineCommunities;
+}
+
+//68
+function useDataAnalysisInDecisionMaking() {
+  let dataAnalysisDecision = document.createElement("div");
+  dataAnalysisDecision.className = "data-analysis-decision";
+
+  return dataAnalysisDecision;
+}
+
+//69
+function createRealTimeFeedbackLoops() {
+  let feedbackLoops = document.createElement("
